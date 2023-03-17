390d80594ec5742522c93867aeb32ff96fe21ab3
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:11:06 -0500

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/specs-scripts/main/ui/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scotdotwtf/specs-scripts/main/ui/main.lua"))()
